867bf339a615fb2fde8c256eb74d9648c2005925
sb <sbenthall@gmail.com>
2013-02-12 19:04:52 -0800

    correcting type signature on classify

--- bayesBuild/bayesBuild.scala
+++ bayesBuild/bayesBuild.scala
@@ -135,7 +135,7 @@ object bayesBuild{
 
   /* classifier takes in a document and the likelihood vectors
    * and returns a classification (an index into the list of loglihoods*/
-  def classify(range : Range, mat : Matrix, priors: List[Double], loglikelihoods : List[BIDMat.FMat]]): Int = { 
+  def classify(indices : FMat, mat : Matrix, priors: List[Double], loglikelihoods : List[BIDMat.FMat]]): Int = { 
   
      return 0;
   }
